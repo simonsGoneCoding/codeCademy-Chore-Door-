cc6c6fcfa4543b0cb1cf31f5be5941adccacb4e7
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-04-10 15:30:51 +0100

    Created logic for the winner.

--- script.js
+++ script.js
@@ -2,30 +2,55 @@
 const doorImage1 = document.getElementById("door1");
 const doorImage2 = document.getElementById("door2");
 const doorImage3 = document.getElementById("door3");
+const startButton = document.getElementById("start");
 const botDoorPath =
   "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 const beachDoorPath =
   "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath =
   "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+const closedDoorPath =
+  "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
 
 // global variables for randomDoorGenerator function
-const numClosedDoors = 3;
+let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
 
+const gameOver = status => {
+  if (status === "win") {
+    startButton.innerHTML = "You win! Play again?";
+  }
+};
+
+const playDoor = () => {
+  numClosedDoors--;
+  if (numClosedDoors === 0) {
+    gameOver("win");
+  }
+};
+
 // events set on all doors
-doorImage1.onclick = () => {
-  doorImage1.src = openDoor1;
+doorImage1.onclick = e => {
+  if (!isClicked(doorImage1)) {
+    doorImage1.src = openDoor1;
+    playDoor();
+  }
 };
 
 doorImage2.onclick = () => {
-  doorImage2.src = openDoor2;
+  if (!isClicked(doorImage2)) {
+    doorImage2.src = openDoor2;
+    playDoor();
+  }
 };
 
 doorImage3.onclick = () => {
-  doorImage3.src = openDoor3;
+  if (!isClicked(doorImage3)) {
+    doorImage3.src = openDoor3;
+    playDoor();
+  }
 };
 
 // functionality:
@@ -60,6 +85,15 @@ const randomDoorGenerator = () => {
       openDoor2 = spaceDoorPath;
       openDoor1 = beachDoorPath;
     }
+  }
+};
+
+//
+const isClicked = door => {
+  if (door.src === closedDoorPath) {
+    return false;
+  } else {
+    return true;
   }
 };
 
